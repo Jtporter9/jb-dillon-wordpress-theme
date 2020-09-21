f738417b2d7a0a511446d790f60027459cb1e0fc
root <root@pop-os.localdomain>
2020-09-20 19:10:47 -0600

    working on custom theme, just adde sass

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php wp_head(); ?>  
+</head>
+<body <?php body_class(); ?> >
+    
+<header>
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
+</header>
